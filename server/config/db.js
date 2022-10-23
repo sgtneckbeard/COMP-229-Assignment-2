ace375a4160be9ea718576c0fbe5c397c8596735
sgtneckbeard <nathanheung@gmail.com>
2022-10-23 00:13:57 -0400

    mongodb atlas failed attempt but left code

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
  {
  "URI":"mongodb://127.0.0.1:27017/book_store"
+ //"URI":"mongodb+srv://nathan:M5r3BmXiiAzj8TCf@mongodbserver.eorljjx.mongodb.net/?retryWrites=true&w=majority"
 }
