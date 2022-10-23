a751f8cee4a3b693feebc88a38d6531c6613e948
sgtneckbeard <nathanheung@gmail.com>
2022-10-23 00:33:17 -0400

    stable build to push to heroku

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports =
  {
- "URI":"mongodb://127.0.0.1:27017/book_store"
- //"URI":"mongodb+srv://nathan:M5r3BmXiiAzj8TCf@mongodbserver.eorljjx.mongodb.net/?retryWrites=true&w=majority"
+ //"URI":"mongodb://127.0.0.1:27017/book_store"
+ "URI": "mongodb+srv://nathan:hjU8uXnVNjOPsGTV@mongodbserver.eorljjx.mongodb.net/book_store?retryWrites=true&w=majority"
 }
