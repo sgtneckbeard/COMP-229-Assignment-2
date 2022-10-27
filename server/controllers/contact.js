33c5f2d2110524adef9d7d3ff6268a41a3a83b81
sgtneckbeard <109317287+sgtneckbeard@users.noreply.github.com>
2022-10-27 08:52:18 -0400

    column cleanup

--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -2,6 +2,7 @@ let express = require ('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 let Contact = require('../model/contacts');
+
 module.exports.displaycontactList = (req,res,next)=>{
     Contact.find((err,contactList)=>{
         if(err)
@@ -41,60 +42,58 @@ module.exports.processAddPage = (req,res,next)=>{
         res.redirect('/contactList');
         }
     });
-    }
+}
     
-        module.exports.displayEditPage = (req,res,next)=>{
-            let id = req.params.id;
-            Contact.findById(id,(err,contactToEdit)=>{
-                if(err)
-                {
-                    console.log(err);
-                    res.end(err);
-                }
-                else
-                {
-                    res.render('contact/edit',{title:'Edit Contact', contact: contactToEdit,
-                    displayName:req.user ? req.user.displayName:''});
-                }
-            
-            });
+module.exports.displayEditPage = (req,res,next)=>{
+    let id = req.params.id;
+    Contact.findById(id,(err,contactToEdit)=>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
         }
+        else
+        {
+            res.render('contact/edit',{title:'Edit Contact', contact: contactToEdit,
+            displayName:req.user ? req.user.displayName:''});
+        }       
+    });
+}
 
-        module.exports.processEditPage = (req,res,next)=>{
-            let id = req.params.id
-            console.log(req.body);
-            let updatedContact = Contact({
-                "_id":id,
-                "name": req.body.name,
-                "number": req.body.number,
-                "email": req.body.email
-            });
-            Contact.updateOne({_id:id}, updatedContact,(err)=>{
-                if(err)
-                {
-                    console.log(err);
-                    res.end(err);
-                }
-                else
-                {
-                    res.redirect('/contactList');
-                }
-            });
+module.exports.processEditPage = (req,res,next)=>{
+    let id = req.params.id
+    console.log(req.body);
+    let updatedContact = Contact({
+        "_id":id,
+        "name": req.body.name,
+        "number": req.body.number,
+        "email": req.body.email
+    });
+    Contact.updateOne({_id:id}, updatedContact,(err)=>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
         }
+        else
+        {
+            res.redirect('/contactList');
+        }
+    });
+}
 
-        module.exports.performDelete= (req,res,next)=>{
-            let id = req.params.id;
-            Contact.remove({_id:id},(err)=>{
-                if(err)
-                {
-                    console.log(err);
-                    res.end(err);
-                }
-                else
-                {
-                    res.redirect('/contactList');
-                }
-                
-            });
-            }
+module.exports.performDelete= (req,res,next)=>{
+    let id = req.params.id;
+    Contact.remove({_id:id},(err)=>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            res.redirect('/contactList');
+        }            
+    });
+}
         
